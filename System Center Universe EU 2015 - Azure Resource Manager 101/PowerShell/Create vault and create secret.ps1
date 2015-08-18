85193cccac9c2d5aaf0d721e819cd63421103ddb
Stijn Callebaut <stijnc@fooji.net>
2015-08-18 22:23:44 +0200

    Keyvault script and powershell demo

---
+++ System Center Universe EU 2015 - Azure Resource Manager 101/PowerShell/Create vault and create secret.ps1
@@ -0,0 +1,39 @@
+﻿import-module azure
+
+
+    #credentials
+$credential = Get-Credential -UserName stijn.callebaut@inovativ.be -Message 'Azure credential'
+
+    #Authenticate
+Add-AzureAccount -Credential $credential
+
+Switch-AzureMode -Name AzureResourceManager
+
+$vaultName = "SCU2015Vault" 
+ $resourceGroup = "SCU2015Vault_RG"
+ $location = "West Europe"
+ $secretName = "scu2015"
+
+ $fileName = "scu2015.pfx"
+ $certPassword = "<PASSWORD>"
+
+ $fileContentBytes = get-content $fileName -Encoding Byte
+ $fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
+
+ $jsonObject = @"
+ {
+ "data": "$filecontentencoded",
+ "dataType" :"pfx",
+ "password": "$certPassword"
+ }
+"@
+
+ $jsonObjectBytes = [System.Text.Encoding]::UTF8.GetBytes($jsonObject)
+ $jsonEncoded = [System.Convert]::ToBase64String($jsonObjectBytes)
+
+ New-AzureResourceGroup -Name $resourceGroup -Location $location
+
+ #make sure the enabled for deployment is checked!
+ New-AzureKeyVault -VaultName $vaultName -ResourceGroupName $resourceGroup -Location $location -sku standard -EnabledForDeployment
+ $secret = ConvertTo-SecureString -String $jsonEncoded -AsPlainText -Force
+ Set-AzureKeyVaultSecret -VaultName $vaultName -Name $secretName -SecretValue $secret
