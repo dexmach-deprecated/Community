5791f0366e5888a4593478a7c66ef66fa84216b7
Stijn Callebaut <stijnc@fooji.net>
2016-03-14 11:07:29 +0100

    Expertslive 2015

---
+++ ExpertsLive-2015/AzureBilling/Add-AzureBillingPowerBI.ps1
@@ -0,0 +1,77 @@
+﻿#region setup
+$clientid = '671c1b4e-097a-4d1c-a050-c841c3871cab'
+$AzureCredential = Get-Credential -username 'stijn.callebaut@inovativ.be' -message 'Azure cred'
+
+$ServiceUri = 'https://billing.inovativ.be' # Unique Id of a Service Principal you must create
+$ServicePassword = 'VaHFuqyCt6kqPvRrN1qqiQg3j5oK5eVo1a9jSggLGO4=' # The password of the Service Principal
+$Password = ConvertTo-SecureString $ServicePassword -AsPlainText -Force
+
+$BillingCred = [PSCredential]::new($ServiceUri,$Passwor)
+
+#endregion
+$Group = 'ExpertsLive'
+	
+#$token		
+$authToken = Get-PBIAuthToken -clientid $clientid -Credential $AzureCredential -verbose
+
+#Validate Group
+$Group  = Get-PBIGroup -authToken $authToken -name 'ExpertsLive' -ErrorAction SilentlyContinue
+Set-PBIGroup -authToken $authToken -name $Group.name
+
+$dataSetSchema = Get-PBIDataSet -authToken $authToken -name "Azure billing" -Verbose
+if( -Not $dataSetSchema){
+    
+    #Create a new schema
+    $dataSetSchema = @{
+         name = "Azure billing"
+         tables = @(
+            @{
+             name = "ResourceConsumption"
+	         columns = @( 
+                    @{name = "UsageStartTime"; dataType = "DateTime"  }
+		            @{name = "UsageEndTime"; dataType = "DateTime"  }
+		            @{name='SubscriptionId'; dataType = 'String'},
+                    @{name='MeterCategory'; dataType = 'String'}
+                    @{name='MeterId'; dataType = 'String'},
+                    @{name='MeterName'; dataType = 'String'},
+                    @{name='MeterSubCategory'; dataType = 'String'},
+                    @{name='MeterRegion'; dataType = 'String'},
+                    @{name='Unit'; dataType = 'String'},
+                    @{name='Quantity'; dataType = 'String'},
+                    @{name='Project'; dataType = 'String'},
+                    @{name='InstanceData'; dataType = 'String'}
+		            )}
+		    , 
+		    @{
+             name = "Pricelist"
+	         columns = @( 
+		            @{name='MeterId'; dataType='String'},
+                    @{name='MeterName'; dataType = 'String'},
+                    @{name='MeterCategory'; dataType = 'String'},
+                    @{name='MeterSubCategory'; dataType = 'String'}
+                    @{name='Unit'; dataType = 'String'},
+                    @{name='MeterTags'; dataType = 'String'},
+                    @{name='MeterRegion'; dataType = 'String'},
+                    @{name='MeterRates'; dataType = 'String'},
+                    @{name='EffectiveDate'; dataType = 'DateTime'},
+                    @{name='IncludedQuantity'; dataType = 'String'},
+                    @{name='Currency'; dataType = 'String'}			
+		            )}
+        )}
+	$dataSetSchema = New-PBIDataSet -authToken $authToken -dataSet $dataSetSchema -defaultRetentionPolicy "basicFIFO" -Verbose
+}
+
+#get the usage data
+$DataUsage = .\Get-iAzureResourceUsageData.ps1 -credential $azureCredential
+
+#Get the pricelist
+$DataPrices = .\Get-iAzureResourceCards.ps1 -Credential $BillingCred
+
+#for demo we clear the data
+Clear-PBITableRows -authToken $authToken -dataSetId $dataSetSchema.Id -tableName 'ResourceConsumption' -Verbose
+Clear-PBITableRows -authToken $authToken -dataSetId $dataSetSchema.Id -tableName 'Pricelist' -Verbose
+
+$DataUsage | Add-PBITableRows -authToken $authToken -dataSetId $dataSetSchema.id -tableName 'ResourceConsumption' -batchSize 1000 -Verbose
+
+
+$DataPrices | Add-PBITableRows -authToken $authToken -dataSetId $dataSetSchema.id -tableName 'Pricelist' -batchSize 1000 -Verbose
---
+++ ExpertsLive-2015/AzureBilling/Get-iAzureResourceCards.ps1
@@ -0,0 +1,44 @@
+﻿#Get-iAzureResourceCards
+    param(
+         $Credential
+        ,$ApiVersion = '2015-06-01-preview'
+        ,$OfferDurableId = 'MS-AZR-0111p' #https://azure.microsoft.com/en-us/support/legal/offer-details/
+        ,$Currency = 'EUR'
+        ,$Locale = 'en-us'
+        ,$RegionInfo = 'BE'
+        ,$AzureTenantId = 'f28deca3-a66e-4902-8d52-7fef383a25d7'
+        ,$SubscriptionId = 'a027c53d-0359-4edf-a3eb-9d5c429cbc95'
+    )
+    # Authentication
+    $AuthenticationCreds = New-object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential($Credential.Username,$Credential.GetNetworkCredential().Password)
+ 
+    $AuthenticationUri = 'https://login.windows.net/{0}' -f $AzureTenantId
+    $authenticationContext = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext($AuthenticationUri,$false)
+ 
+    $resource = 'https://management.core.windows.net/'
+    $authenticationResult = $authenticationContext.AcquireToken($resource, $AuthenticationCreds)
+ 
+    $ResHeaders = @{'authorization' = $authenticationResult.CreateAuthorizationHeader()}
+
+    $ResourceCard = "https://management.azure.com/subscriptions/{5}/providers/Microsoft.Commerce/RateCard?api-version={0}&`$filter=OfferDurableId eq '{1}' and Currency eq '{2}' and Locale eq '{3}' and RegionInfo eq '{4}'" -f $ApiVersion, $OfferDurableId, $Currency, $Locale, $RegionInfo, $SubscriptionId
+
+    $resources = Invoke-RestMethod -Uri $ResourceCard -Headers $ResHeaders -ContentType 'application/json' 
+    $data = $Resources.Meters | Select-Object `
+                            MeterId `
+                            ,MeterName `
+                            ,MeterCategory `
+                            ,MeterSubCategory `
+                            ,Unit `
+                            ,MeterTags `
+                            ,MeterRegion `
+                            ,@{n='MeterRates';e={$_.MeterRates.0}} `
+                            ,EffectiveDate `
+                            ,IncludedQuantity `
+                            ,@{n='Currency'; e={$Currency}}
+    $data
+    
+    $ClientID = '54763718-b7b2-4798-8929-037d42e9600f'
+
+
+
+ 
